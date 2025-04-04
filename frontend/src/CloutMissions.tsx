6f5439ce875d0fc86dddfdf98c4bfffe5807e496
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-04-04 20:00:18 +0100

    Create CloutMissions.tsx

---
+++ frontend/src/CloutMissions.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
+const CloutMissions = () => {
+  return (
+    <div>
+      <h2>Clout Missions</h2>
+      <p>Mission: Get 50 Likes in 24 Hours for 100 Coins</p>
+      <p>Check the Hall of Fame to see top users!</p>
+    </div>
+  );
+};
+
+export default CloutMissions;
