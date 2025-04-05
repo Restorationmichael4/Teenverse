90bd043b62079a82e636e861e9e6deefc92e2739
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-04-05 16:59:49 +0100

    Update posts.ts

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import db from './database'; // or wherever your SQLite setup is
+import db from '../database'; // should be correct if in routes/
 const router = express.Router();
 
 router.get('/newsfeed', async (req, res) => {
