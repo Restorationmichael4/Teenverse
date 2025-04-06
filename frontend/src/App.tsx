183343146ba7dffc495e587f42c643d50161d381
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-04-06 07:32:43 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Register from "./Register";
-import Login from "./Login";
-
-export default function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </Router>
-    );
-}
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CoinFlip from "./pages/CoinFlip";
-
-function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/coin-flip" element={<CoinFlip userId={1} />} />
-            </Routes>
-        </Router>
-    );
-}
-// src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
@@ -39,6 +11,7 @@ import UltimateShowdown from './pages/UltimateShowdown';
 import CloutMissions from './pages/CloutMissions';
 import HallOfFame from './pages/HallOfFame';
 import Login from './pages/Login';
+import Register from './Register';
 import { useAuth } from './hooks/useAuth'; // Custom hook for user auth
 
 const App = () => {
@@ -49,25 +22,26 @@ const App = () => {
       {isLoggedIn ? (
         <>
           <Navigation />
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/rant-zone" component={RantZone} />
-            <Route path="/game-squad" component={GameSquad} />
-            <Route path="/coin-flip" component={CoinFlip} />
-            <Route path="/hype-battles" component={HypeBattles} />
-            <Route path="/ultimate-showdown" component={UltimateShowdown} />
-            <Route path="/clout-missions" component={CloutMissions} />
-            <Route path="/hall-of-fame" component={HallOfFame} />
-            <Redirect from="/" to="/home" />
-          </Switch>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/rant-zone" element={<RantZone />} />
+            <Route path="/game-squad" element={<GameSquad />} />
+            <Route path="/coin-flip" element={<CoinFlip />} />
+            <Route path="/hype-battles" element={<HypeBattles />} />
+            <Route path="/ultimate-showdown" element={<UltimateShowdown />} />
+            <Route path="/clout-missions" element={<CloutMissions />} />
+            <Route path="/hall-of-fame" element={<HallOfFame />} />
+          </Routes>
         </>
       ) : (
-        <Route path="/" component={Login} />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       )}
     </Router>
   );
 };
 
-export default App;
 export default App;
