dcd88d13d00d289bde7ac28024b91a5fc17bc31a
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-04-08 10:34:44 +0100

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "./hooks/useAuth";
+import { useAuth } from "../hooks/useAuth";
 
 export default function Login() {
     const [email, setEmail] = useState("");
