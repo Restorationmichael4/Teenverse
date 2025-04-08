8062b95f166a9d3c399df84342a899364247d410
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-04-08 11:15:58 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./frontend/src/**/*.{js,jsx,ts,tsx}",
-        "./frontend/src/pages/**/*.{js,jsx,ts,tsx}",
-        "./frontend/src/hooks/**/*.{js,jsx,ts,tsx}"
+        "./frontend/src/**/*.{js,jsx,ts,tsx}", // Include all files in src
+        "./frontend/src/pages/**/*.{js,jsx,ts,tsx}", // Include files in pages
+        "./frontend/src/hooks/**/*.{js,jsx,ts,tsx}" // Include files in hooks
     ],
     theme: {
         extend: {},
